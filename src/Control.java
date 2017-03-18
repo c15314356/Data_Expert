e67e7ceba5d7ea6fa35ebe848205577eea7313af
c15314356 <c15314356@mydit.ie>
2017-03-18 21:26:58 +0000

    Moved all database related functions into its own class
    Recreated a main control method and now allowed users to input their own
    queries

--- src/Control.java
+++ src/Control.java
@@ -1,39 +1,18 @@
-import java.io.File;
-import java.sql.*;
 import java.util.Scanner;
 
-public class Control {
+public class Control 
+{
 
-	public static void main(String[] args) 
-	
+	public static void main(String[] args)
 	{
-		String uname="Builder2";
-		String upass="br0adband1";
-		try 
-		{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection  c=DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe",uname,upass);
-			Statement func = c.createStatement();
-			ResultSet rset = func.executeQuery("SELECT * FROM JOBS");
-			
-			while (rset.next())
-			{
-				for (int i=0; i<5;i++)
-				{
-					System.out.print(rset.getString(i+1)+"\t");
-				}
-				System.out.print("\n");
-			}
-		} 
-		catch (ClassNotFoundException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
+		//asks for what query the user would like to run
+		System.out.println("Please enter a query you would like to run....");
+		Scanner ans=new Scanner(System.in);
+		String func=ans.nextLine();
+		ans.close();
 		
+		//runs DatabaseReader class
+		DatabaseReader data=new DatabaseReader(func);
+		data.Connect();
 	}
-
 }
--- src/DatabaseReader.java
+++ src/DatabaseReader.java
@@ -1,5 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseReader 
 {
+	private String uname;
+	private String upass;
+	private String statement;
 	
+	public DatabaseReader(String statement)
+	{
+		this.uname="Builder2";
+		this.upass="br0adband1";
+		this.statement=statement;
+	}
+	
+	public void Connect()
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe",uname,upass);
+			Statement func = con.createStatement();
+			ResultSet results = func.executeQuery(statement);
+			
+			while (results.next())
+			{
+				for (int i=0; i<13;i++)
+				{
+					System.out.print(results.getString(i+1)+"\t");
+				}
+				System.out.print("\n");
+			}
+		}
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
+		} 
+	}
 }
