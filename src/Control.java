eef5a3c57dce271c5f3d58687a06d9a91051926c
c15314356 <c15314356@mydit.ie>
2017-03-18 17:26:38 +0000

    Added ability to connect to a database and so queries

--- src/Control.java
+++ src/Control.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Control {
@@ -11,9 +12,37 @@ public class Control {
 		//String name =new String(path.nextLine());
 		
 		//FileReader file= new FileReader(name);
-		FileReader file= new FileReader("C:\\g1.csv");
+		//FileReader file= new FileReader("C:\\g1.csv");
 		
-		file.readFile();
+		//file.readFile();
+		
+		String uname="Builder2";
+		String upass="br0adband1";
+		try 
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection  c=DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe",uname,upass);
+			Statement func = c.createStatement();
+			ResultSet rset = func.executeQuery("SELECT * FROM CORDER");
+			
+			while (rset.next())
+			{
+				for (int i=0; i<5;i++)
+				{
+					System.out.print(rset.getString(i+1)+"\t");
+				}
+				System.out.print("\n");
+			}
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
 	}
 
 }
