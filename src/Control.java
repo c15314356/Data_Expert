9624374b27f012b7b639d423acdd9907bc650716
c15314356 <c15314356@mydit.ie>
2017-03-20 19:00:19 +0000

    Testing dynamic ways to store results from queries to allow multiple queries to be executed in the database
    just testing for a way in which will allow dynamic results from
    databases

--- src/Control.java
+++ src/Control.java
@@ -1,10 +1,14 @@
+//
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Control 
 {
-
 	public static void main(String[] args)
-	{
+	{	
+		/*
 		//asks for what query the user would like to run
 		System.out.println("Please enter a query you would like to run....");
 		Scanner ans=new Scanner(System.in);
@@ -14,5 +18,24 @@ public class Control
 		//runs DatabaseReader class
 		DatabaseReader data=new DatabaseReader(func);
 		data.Connect();
+		*/
+		
+		List<String> myarray= new ArrayList<String>();
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("Enter in a String");
+			Scanner temp=new Scanner(System.in);
+			String ans=temp.nextLine();
+			myarray.add(ans);
+			temp.close();
+		}
+		
+		for (String element:myarray)
+		{
+			System.out.println(element);
+			
+			
+		}
+		
 	}
 }
